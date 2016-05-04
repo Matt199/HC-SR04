9b9835aafaae22d8b0f0aca77dd2bed68d9c9fe5
Mateusz Kopacz <Matt@iMac-Mateusz.local>
2016-05-04 12:33:49 +0200

    Add distance in inch

--- HC-SR04/HC-SR04.ino
+++ HC-SR04/HC-SR04.ino
@@ -1,32 +1,35 @@
-#define ECHO 2
-#define TRIG 3 
+#define ECHO 2 // define digital pin for Echo as 2
+#define TRIG 3  // define digital pin for Trig as 3
 
-int Time;
+int Time; 
 int distanceInCm;
+int distanceInInch;
 
 void setup()
 {
-  pinMode(ECHO, INPUT);
-  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT); // echo as Input
+  pinMode(TRIG, OUTPUT); // trig as Output
   Serial.begin(9600);
   
 }
 
-void counting_The_Distance()
+void counting_The_Distance() // creating the function for couting the distance
 {
   digitalWrite(TRIG, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG, LOW);
-  Time = pulseIn(ECHO, HIGH);
-  distanceInCm = Time/58;
-  
+  Time = pulseIn(ECHO, HIGH); // pulseIn returns time in micro seconds
+  distanceInCm = Time / 58; // calculating the distance in cm
+  distanceInInch = Time / 148; // calculating the distance in Inch
 }
 
 void loop()
 {
-  counting_The_Distance();
+  counting_The_Distance(); // init func
   Serial.print("Distance: ");
   Serial.print(distanceInCm);
+  //Serial.print(distanceInInch);
   Serial.println(" cm");
-  delay(500);
+  //Serial.println(" Inch");
+  delay(500); 
 }
